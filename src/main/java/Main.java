a1cc5e714032a320af37b4637c203d3edf4aecd9
URGG <georgeurgiles2@gmail.com>
2024-12-30 11:51:35 -0800

    [any message]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,51 @@
+import client.SocketClient;
+import kafka.Kafka;
+import kafka.KafkaLoader;
+import store.Storage;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.err.println("Logs from your program will appear here!");
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.ThreadFactory;
 
-    ServerSocket serverSocket = null;
-    Socket clientSocket = null;
-    int port = 9092;
-    try {
-      serverSocket = new ServerSocket(port);
-      // Since the tester restarts your program quite often, setting SO_REUSEADDR
-      // ensures that we don't run into 'Address already in use' errors
-      serverSocket.setReuseAddress(true);
-      // Wait for connection from client.
-      clientSocket = serverSocket.accept();
-    } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
-    } finally {
-      try {
-        if (clientSocket != null) {
-          clientSocket.close();
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        final ThreadFactory threadFactory = Thread.ofVirtual().factory();
+        final Storage storage = new Storage();
+
+        final Kafka kafka = new Kafka();
+        Path path = null;
+
+
+        if (args.length == 1) {
+
+            System.out.println(args[0]);
+
+            path = Paths.get(args[0]);
+
+            if (Files.exists(path)) KafkaLoader.load(path, storage);
         }
-      } catch (IOException e) {
-        System.out.println("IOException: " + e.getMessage());
-      }
+
+        final int port = 9092;
+
+        System.out.println("port: %s".formatted(port));
+
+        try (final ServerSocket serverSocket = new ServerSocket(port)) {
+            serverSocket.setReuseAddress(true);
+
+            while (true) {
+                final Socket socket = serverSocket.accept();
+                final var client = new SocketClient(socket, kafka);
+
+                final Thread thread = threadFactory.newThread(client);
+                thread.start();
+            }
+        }
     }
-  }
+
 }
